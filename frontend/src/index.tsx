e3a89affe73cd81efa5400752831cf0dda0ad648
Amrit Raj <amrit.raj1504@gmail.com>
2024-01-08 22:45:21 +0530

    react-query implementation started

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,21 +1,33 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import { Provider } from 'react-redux';
-import { store } from './redux/store';
-import { SnackbarProvider } from 'notistack';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { Provider } from "react-redux";
+import { store } from "./redux/store";
+import { SnackbarProvider } from "notistack";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+// const queryClient = new QueryClient();
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 20000,
+    },
+  },
+});
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById("root") as HTMLElement
 );
 root.render(
-  <Provider store={store} >
-    <SnackbarProvider autoHideDuration={2000} >
-      <App />
-    </SnackbarProvider>
-  </Provider>
+  <QueryClientProvider client={queryClient} >
+    <Provider store={store}>
+      <SnackbarProvider autoHideDuration={2000}>
+        <App />
+      </SnackbarProvider>
+    </Provider>
+  </QueryClientProvider>
 );
 
 // If you want to start measuring performance in your app, pass a function
